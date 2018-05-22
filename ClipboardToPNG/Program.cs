eab8677b625a10b14514d9c920033abb88deabdd
Ezhik <i@ezhik.me>
2018-05-22 17:02:56 -0400

    Clean up logic and add some explanation for the magic values

--- ClipboardToPNG/Program.cs
+++ ClipboardToPNG/Program.cs
@@ -17,10 +17,6 @@ namespace ClipboardToPNG
         static int Main()
         {
             var data = Clipboard.GetDataObject() as DataObject;
-            if (!data.ContainsImage() && !data.GetDataPresent(DataFormats.Dib))
-            {
-                return 1;
-            }
 
             string path = Environment.ExpandEnvironmentVariables(@"%TEMP%\clip.png");
             //foreach (var i in data.GetFormats())
@@ -42,7 +38,7 @@ namespace ClipboardToPNG
                 }
             }
 
-            // if we have DIB, convert to an image and then save;
+            // if we have DIB that's 32 bit (ARGB), convert to an image and then save
             if (data.GetDataPresent(DataFormats.Dib))
             {
                 Debug.Print("dib got");
@@ -57,12 +53,19 @@ namespace ClipboardToPNG
                     {
                         var test = BitConverter.ToInt64(dib, offset);
                         var test2 = BitConverter.ToInt32(dib, offset + 8);
-                        //Debug.Print(test + "" + test2);
+                        // some applications include this bitmask of some sorts while copying that is 12 bytes in size. 
+                        // the bmp header would let us get the offset to the pixel array directly, but we only have access to the dib header
+                        // so the best i could come up with here was to check for this bitmask
+                        // this means that there's a chance that an image with pixels that happen to hit these values could end up getting offset instead
+                        // but this should be relatively rare - it would require a hit on:
+                        // (A, R, G, B) = {(0, 0, 255, 0), (0, 255, 0, 0), (255, 0, 0, 0)}, which would be
+                        // a sequence of a transparent green pixel, a transparent red pixel, and of an opaque black pixel.
                         if (test == 0x0000ff0000ff0000 && test2 == 0x000000ff)
                         {
                             offset += 12;
                         }
-                            var gch = GCHandle.Alloc(dib, GCHandleType.Pinned);
+
+                        var gch = GCHandle.Alloc(dib, GCHandleType.Pinned);
                         Bitmap bmp = null;
                         try
                         {
